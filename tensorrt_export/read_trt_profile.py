0e1169e58f961c189d5025f1d25c89cf687e7cce
Tlntin <TlntinDeng01@Gmail.com>
2023-06-01 16:26:30 +0800

    fixup some bug for tensorRT read

--- tensorrt_export/read_trt_profile.py
+++ tensorrt_export/read_trt_profile.py
@@ -5,7 +5,7 @@ import os
 now_dir = os.path.dirname(os.path.abspath(__file__))
 project_dir = os.path.dirname(now_dir)
 model_dir = os.path.join(project_dir, "models")
-trt_enginge_path = os.path.join(model_dir, "chatglm6b-bs1.plan")
+trt_enginge_path = os.path.join(model_dir, "chatglm6b-bs1-18.5G.plan")
 
 # Load TensorRT Engine
 with open(trt_enginge_path, 'rb') as f, trt.Runtime(trt.Logger(trt.Logger.WARNING)) as runtime:
@@ -13,9 +13,14 @@ with open(trt_enginge_path, 'rb') as f, trt.Runtime(trt.Logger(trt.Logger.WARNIN
 
 # print input and output
 print("Inputs and Output:")
-for i in range(engine.num_bindings):
+n_io = engine.num_io_tensors
+tensor_names = []
+tensor_modes = []
+for i in range(n_io):
     tensor_name = engine.get_tensor_name(i)
+    tensor_names.append(tensor_name)
     tensor_mode = engine.get_tensor_mode(tensor_name)
+    tensor_modes.append(tensor_mode)
     tensor_shape = engine.get_tensor_shape(tensor_name)
     if tensor_mode == trt.TensorIOMode.INPUT:
         print(f"input_name:'{tensor_name}', input_shape: {tensor_shape}", )
@@ -23,26 +28,17 @@ for i in range(engine.num_bindings):
         print(f"output_name:'{tensor_name}', input_shape: {tensor_shape}", )
 
 
-# print profile
-# print("Profile configuration:")
-# for i in range(engine.num_optimization_profiles):
-#     print(" - profile index: ", i)
-#     for j in range(engine.num_bindings):
-#         tensor_name = engine.get_tensor_name(j)
-#         tensor_mode = engine.get_tensor_mode(tensor_name)
-#         tensor_shape = engine.get_tensor_shape(tensor_name)
-#         if tensor_mode == trt.TensorIOMode.INPUT:
-#             print("- input name: ", tensor_name)
-#             print("min|opt|max shape: ", engine.get_tensor_profile_shape(tensor_name, i))
-#             # print("min|opt|max shape: ", engine.get_profile_shape(i, j))
-
 print("Profile configuration:")
 for i in range(engine.num_optimization_profiles):
+    print('=' * 20) 
     print(" - profile index: ", i)
-    for j in range(engine.num_bindings):
-        if engine.binding_is_input(j):
-            profile = engine.get_profile_shape(i, j)
-            print("   - input name: ", engine.get_binding_name(j))
+    for j in range(n_io):
+        tensor_name = tensor_names[j]
+        tensor_mode = tensor_modes[j]
+        if tensor_mode == trt.TensorIOMode.INPUT:
+            # profile = engine.get_profile_shape(i, j)
+            profile = engine.get_tensor_profile_shape(tensor_name, i)
+            print("   - input name: ", tensor_name)
             print("     min shape: ", profile[0])
-            print("     max shape: ", profile[1])
-            print("     optimal shape: ", profile[2])
+            print("     opt shape: ", profile[1])
+            print("     max shape: ", profile[2])
