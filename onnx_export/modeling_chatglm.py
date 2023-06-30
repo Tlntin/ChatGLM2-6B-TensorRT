bd55117b09d5eb7c39db642d47966e5af94be42a
Tlntin <TlntinDeng01@Gmail.com>
2023-06-30 11:53:17 +0800

    fixup some bug for TensorRT check

--- onnx_export/modeling_chatglm.py
+++ onnx_export/modeling_chatglm.py
@@ -846,7 +846,7 @@ class ChatGLMForConditionalGeneration(ChatGLMPreTrainedModel):
     def __init__(self, config: ChatGLMConfig, empty_init=True, device=None):
         super().__init__(config)
 
-        self.max_sequence_length = config.max_input_length
+        self.max_sequence_length = config.max_length
         self.transformer = ChatGLMModel(config, empty_init=empty_init, device=device)
         self.config = config
         self.quantized = False
